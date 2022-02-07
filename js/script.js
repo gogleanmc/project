6e6b017673c18def55774f60e3899cc309956290
den <den@iMac-den.local>
2022-02-07 20:43:57 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+const a = true, 
+    b = false;
+
+console.log(a && b); 
+// answers[0] =  prompt('Как ваше имя?','');
+// answers[1] = prompt('Как ваша фамилия?','');
+// answers[2] = prompt('Сколько вам лет?','');
+
+// document.write(answers);
+
+// const answer = +prompt("Вам есть 18", "18");
+// console.log(typeof(answer));
+// const result = confirm("Are you here?");
+// console.log(result);
+
+
+// let number = 5.5;
+
+// console.log(-5/0);
+// console.log('string' * 9);
+
+// const obg = {
+//     name: 'j',
+//     age: 25,
+//     isMarried: false,
+//     cat: 22
+// };
+
+// console.log(obg.name);
+// console.log(obg['name']);
+
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+// {
+//     var result2 = 50;
+// }
+
+// console.log(result2);
